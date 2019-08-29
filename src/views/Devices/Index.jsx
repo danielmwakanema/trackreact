39d87a34db86f0dd4b03bedf59da52a74760ca16
danielmwakanema <danielmwakanema95@gmail.com>
2019-08-29 10:56:32 +0200

    chore(Devices): Init components

---
+++ src/views/Devices/Index.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Table,
+  Row,
+  Col
+} from "reactstrap";
+
+import './Index.css';
+
+import TraccarAPI from '../../lib/TraccarAPI';
+import DeviceService from '../../Services/DeviceService';
+
+const service = new DeviceService(TraccarAPI);
+
+class Index extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      devices: []
+    }
+  }
+
+  async componentDidMount () {
+    await this.getDevices()
+  }
+
+  async getDevices () {
+    const devices = await service.all()
+    console.log(devices)
+  }
+
+  render() {
+    return (
+      <>
+        <div className="content">
+          <Row>
+            <Col md="12">
+              <Card className="card-plain">
+                <CardHeader>
+                  <CardTitle tag="h4">Device List</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Table className="tablesorter" responsive>
+                    <thead className="text-primary">
+                      <tr>
+                        <th>Name</th>
+                        <th>Country</th>
+                        <th>City</th>
+                        <th className="text-center">Salary</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Dakota Rice</td>
+                        <td>Niger</td>
+                        <td>Oud-Turnhout</td>
+                        <td className="text-center">$36,738</td>
+                      </tr>
+                      <tr>
+                        <td>Minerva Hooper</td>
+                        <td>Curaçao</td>
+                        <td>Sinaai-Waas</td>
+                        <td className="text-center">$23,789</td>
+                      </tr>
+                      <tr>
+                        <td>Sage Rodriguez</td>
+                        <td>Netherlands</td>
+                        <td>Baileux</td>
+                        <td className="text-center">$56,142</td>
+                      </tr>
+                      <tr>
+                        <td>Philip Chaney</td>
+                        <td>Korea, South</td>
+                        <td>Overland Park</td>
+                        <td className="text-center">$38,735</td>
+                      </tr>
+                      <tr>
+                        <td>Doris Greene</td>
+                        <td>Malawi</td>
+                        <td>Feldkirchen in Kärnten</td>
+                        <td className="text-center">$63,542</td>
+                      </tr>
+                      <tr>
+                        <td>Mason Porter</td>
+                        <td>Chile</td>
+                        <td>Gloucester</td>
+                        <td className="text-center">$78,615</td>
+                      </tr>
+                      <tr>
+                        <td>Jon Porter</td>
+                        <td>Portugal</td>
+                        <td>Gloucester</td>
+                        <td className="text-center">$98,615</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Index;
