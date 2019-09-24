784b3336b7a7b9f2cc6f249d9753653255a2ad2d
danielmwakanema <danielmwakanema95@gmail.com>
2019-09-24 11:05:09 +0200

    feat(Components): Add group list

---
+++ src/views/Groups/Index.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { connect } from "react-redux";
+
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Table,
+  Row,
+  Col
+} from "reactstrap";
+
+import { userGroups } from "../../Redux/actions/groupActions";
+
+import "./Index.css";
+
+class Index extends React.Component {
+  componentDidMount() {
+    this.props.userGroups();
+  }
+
+  render() {
+    return (
+      <>
+        <div className="content">
+          <Row>
+            <Col md="12">
+              <Card className="card-plain">
+                <CardHeader>
+                  <CardTitle tag="h4">Group List</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  {this.props.groups.length > 0 && (
+                    <Table className="tablesorter" responsive>
+                      <thead className="text-primary">
+                        <tr>
+                          <th>Name</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <>
+                          {this.props.groups.map(group => {
+                            return (
+                              <tr key={group.id}>
+                                <td>{group.name}</td>
+                              </tr>
+                            );
+                          })}
+                        </>
+                      </tbody>
+                    </Table>
+                  )}
+                  {this.props.groups.length === 0 && (
+                    <p>You have no groups at the moment.</p>
+                  )}
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    groups: state.Group.groups
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    userGroups: () => dispatch(userGroups())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Index);
