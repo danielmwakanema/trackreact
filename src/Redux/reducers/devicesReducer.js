33e639179f835dd4986e8c6e439b6448f3d1477a
danielmwakanema <danielmwakanema95@gmail.com>
2019-10-03 15:00:00 +0200

    refactor(Reducers): Add more device action types

--- src/Redux/reducers/devicesReducer.js
+++ src/Redux/reducers/devicesReducer.js
@@ -1,9 +1,13 @@
 import {
   ADD_DEVICE_SUCCESS,
   ADD_DEVICE_FAILED,
+  UPDATE_DEVICE_SUCCESS,
+  UPDATE_DEVICE_FAILED,
   GET_USER_DEVICES_SUCCESS,
   GET_USER_DEVICES_FAILED,
-  RESET_USER_DEVICES_LIST
+  RESET_USER_DEVICES_LIST,
+  VIEW_DEVICE_SUCCESS,
+  VIEW_DEVICE_FAILED
 } from "../actions/actionTypes";
 
 const init = {
@@ -17,12 +21,20 @@ export default (state = init, action) => {
       return Object.assign({}, state);
     case ADD_DEVICE_FAILED:
       return Object.assign({}, state);
+    case UPDATE_DEVICE_SUCCESS:
+      return Object.assign({}, state);
+    case UPDATE_DEVICE_FAILED:
+      return Object.assign({}, state);
     case GET_USER_DEVICES_SUCCESS:
       return Object.assign({}, state, action.payload);
     case GET_USER_DEVICES_FAILED:
       return Object.assign({}, state);
     case RESET_USER_DEVICES_LIST:
       return Object.assign({}, state, { devices: [] });
+    case VIEW_DEVICE_SUCCESS:
+      return Object.assign({}, state, action.payload);
+    case VIEW_DEVICE_FAILED:
+      return Object.assign({}, state);
     default:
       return state;
   }
