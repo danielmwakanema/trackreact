2e2063de3c0745f441740c86da5751cead9bd464
danielmwakanema <danielmwakanema95@gmail.com>
2019-10-03 15:05:33 +0200

    refactor(Routes): Add + group and geofence routes

--- src/Routes/Devices.js
+++ src/Routes/Devices.js
@@ -1,5 +1,8 @@
 import Add from '../views/Devices/Add';
 import Index from '../views/Devices/Index';
+import View from '../views/Devices/View';
+import AddToGroup from '../views/Devices/AddToGroup';
+import AddToGeofence from '../views/Devices/AddToGeofence';
 
 export default [
   {
@@ -7,6 +10,27 @@ export default [
     name: "Add Device",
     icon: "tim-icons icon-simple-add",
     component: Add,
+    layout: "/admin"
+  },
+  {
+    path: "/device/addToGroup/:id",
+    name: "Add Device To Group",
+    icon: "tim-icons icon-simple-add",
+    component: AddToGroup,
+    layout: "/admin"
+  },
+  {
+    path: "/device/addToGeofence/:id",
+    name: "Add Device To Geofence",
+    icon: "tim-icons icon-simple-add",
+    component: AddToGeofence,
+    layout: "/admin"
+  },
+  {
+    path: "/device/view/:id",
+    name: "View Device",
+    icon: "tim-icons icon-simple-add",
+    component: View,
     layout: "/admin"
   },
   {
