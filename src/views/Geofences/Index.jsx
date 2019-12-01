8a09cef4f6fa63273f8a321069e2a5c7c9a82130
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-01 09:16:33 +0200

    refactor(Geofences): Add link to add

--- src/views/Geofences/Index.jsx
+++ src/views/Geofences/Index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 // reactstrap components
 import {
@@ -30,6 +31,7 @@ class Index extends React.Component {
               <Card className="card-plain">
                 <CardHeader>
                   <CardTitle tag="h4">Geofence List</CardTitle>
+                  <Link to={`/admin/geofence/add`}>+ Geofence</Link>
                 </CardHeader>
                 <CardBody>
                   {this.props.geofences.length > 0 && (
