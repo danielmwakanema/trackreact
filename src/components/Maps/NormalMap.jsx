3e46202f65bbc07452776d7590a5967b60b3ae21
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-01 09:13:55 +0200

    refactor(Maps): Inject locations into normal map

--- src/components/Maps/NormalMap.jsx
+++ src/components/Maps/NormalMap.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import { withScriptjs } from 'react-google-maps/lib';
 
+const CENTER = { lat: -14.0006186, lng: 33.7617994 }
+const ZOOM = 5
+
+const validCoordinates = (lat, lng) => lat && lng
+
+
 export default withScriptjs(
   withGoogleMap(props => (
-    <GoogleMap defaultZoom={15} center={props.center}>
-      {props.center.lat && props.center.lng && (
-        <Marker position={props.center} />
-      )}
+    <GoogleMap defaultZoom={ZOOM} center={CENTER}>
+      { props.devices.length >= 0 && (props.devices.map((device, index) =>
+        validCoordinates(device.lat, device.lng && <Marker key={index} position={{ lat: device.lat, lng: device.lng }} title={`${device.name}`} />)))
+      }
     </GoogleMap>
   ))
 );
