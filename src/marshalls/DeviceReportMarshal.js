ce3b9b298fae89b628b2ea33f7ead61d911671d1
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-07 16:26:42 +0200

    fix(Reports): Fix missing trip duration average

--- src/marshalls/DeviceReportMarshal.js
+++ src/marshalls/DeviceReportMarshal.js
@@ -7,7 +7,7 @@ const DeviceReportMarshall = (report) => {
       speedAverage: avgSpeed(),
       fuelAverage: avgFuelUsed(),
       distanceTotal: totalDistance(),
-      durationAvergae: avgDuration(),
+      durationAverage: avgDuration(),
       maxSpeedAverage: avgMaxSpeed(),
       speedTrends: tripSpeedTrend(),
       fuelTrends: fuelUsageTrend(),
@@ -19,7 +19,7 @@ const DeviceReportMarshall = (report) => {
   const avgSpeed = () => Math.round(data.reduce((acc, val) => acc + val.averageSpeed, 0) / data.length)
   const avgFuelUsed = () => Math.round(data.reduce((acc,  val) => acc + val.spentFuel, 0) / data.length)
   const totalDistance = () => Math.round(data.reduce((acc, val) => acc + val.distance, 0))
-  const avgDuration = () => Math.round(data.reduce((acc, val) => acc + val.duration, 0) / data.length)
+  const avgDuration = () => Math.round(data.reduce((acc, val) => acc + (val.duration / 3600), 0) / data.length)
   const avgMaxSpeed = () => Math.round(data.reduce((acc, val) => acc + val.maxSpeed, 0) / data.length)
   const tripSpeedTrend = () => data.map(datum => Math.round(datum.averageSpeed))
   const fuelUsageTrend = () => data.map(datum => Math.round(datum.spentFuel))
