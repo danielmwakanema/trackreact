5a92f5996f2c3ea1041912d6a12ba36d531370da
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-11 14:05:33 +0200

    chore(Marshalls): Add stop report marshall

---
+++ src/marshalls/SummaryReportMarshal.js
@@ -0,0 +1,20 @@
+const SummaryReportMarshall = (report) => {
+  const data = report
+
+  const generateMarshall = () => {
+    return data.map(deviceSummary => Object.assign({}, {
+      name: deviceSummary.deviceName,
+      maxSpeed: deviceSummary.maxSpeed,
+      averageSpeed: deviceSummary.averageSpeed,
+      distance: deviceSummary.distance,
+      fuel: deviceSummary.spentFuel,
+      engineHours: deviceSummary.engineHours
+    }))
+  }
+
+  return {
+    generateMarshall
+  }
+}
+
+export default SummaryReportMarshall;
