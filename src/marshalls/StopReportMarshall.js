cfcedee68b9a89ae0ab83ac703a4b65255cd2358
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-12 10:12:52 +0200

    chore(Reducers): Cosmetic changes

--- src/marshalls/StopReportMarshall.js
+++ src/marshalls/StopReportMarshall.js
@@ -1,26 +1,31 @@
-import moment from 'moment';
+import moment from "moment";
 
-const StopReportMarshall = (report) => {
-  const data = report
+const StopReportMarshall = report => {
+  const data = report;
 
   const generateMarshall = () => {
-    return data.map(stop => Object.assign({}, {
-      name: stop.deviceName,
-      duration: stop.duration,
-      start: time(stop.startTime),
-      end: time(stop.endTime),
-      lat: stop.latitude,
-      lon: stop.longitude,
-      fuel: stop.spentFuel,
-      engineHours: stop.engineHours
-    }))
-  }
+    return data.map(stop =>
+      Object.assign(
+        {},
+        {
+          name: stop.deviceName,
+          duration: stop.duration,
+          start: time(stop.startTime),
+          end: time(stop.endTime),
+          lat: stop.latitude,
+          lon: stop.longitude,
+          fuel: stop.spentFuel,
+          engineHours: stop.engineHours
+        }
+      )
+    );
+  };
 
-  const time = stamp => moment(stamp).format('YYYY-MM-DD H:m:s')
+  const time = stamp => moment(stamp).format("YYYY-MM-DD H:m:s");
 
   return {
     generateMarshall
-  }
-}
+  };
+};
 
 export default StopReportMarshall;
