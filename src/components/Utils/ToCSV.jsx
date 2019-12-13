64dd81d5cb19721fd556416ab5f654f8ca922756
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-13 13:12:49 +0200

    refactor(Utils): Fix ToCSV button bug

--- src/components/Utils/ToCSV.jsx
+++ src/components/Utils/ToCSV.jsx
@@ -5,9 +5,7 @@ import { Button } from 'reactstrap';
 class ToCSV extends React.Component {
   render() {
     return (
-      <Button>
-        <CSVLink data={this.props.data} filename={this.props.filename}>Export CSV</CSVLink>
-      </Button>
+      <CSVLink data={this.props.data} filename={this.props.filename}>Export CSV</CSVLink>
     )
   }
 }
