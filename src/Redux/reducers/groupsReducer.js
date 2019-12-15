25df9399bb2eb1e0377e161489915ebee47029a4
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-15 14:03:39 +0200

    refactor(Reducers): Drop unused paths

--- src/Redux/reducers/groupsReducer.js
+++ src/Redux/reducers/groupsReducer.js
@@ -1,8 +1,5 @@
 import {
-  ADD_GROUP_SUCCESS,
-  ADD_GROUP_FAILED,
   GET_USER_GROUPS_SUCCESS,
-  GET_USER_GROUPS_FAILED,
   RESET_USER_GROUPS_LIST
 } from "../actions/actionTypes";
 
@@ -13,14 +10,8 @@ const init = {
 
 export default (state = init, action) => {
   switch (action.type) {
-    case ADD_GROUP_SUCCESS:
-      return Object.assign({}, state);
-    case ADD_GROUP_FAILED:
-      return Object.assign({}, state);
     case GET_USER_GROUPS_SUCCESS:
       return Object.assign({}, state, { groups: action.payload });
-    case GET_USER_GROUPS_FAILED:
-      return Object.assign({}, state);
     case RESET_USER_GROUPS_LIST:
       return Object.assign({}, state, { groups: [] });
     default:
