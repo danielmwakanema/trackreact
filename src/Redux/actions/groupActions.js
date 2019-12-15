a94020e39a02902ff96a5532ee3c637e9fdce4ad
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-15 14:02:14 +0200

    refactor(Actions): Use generic request actions

--- src/Redux/actions/groupActions.js
+++ src/Redux/actions/groupActions.js
@@ -1,67 +1,35 @@
 import TraccarAPI from "../../lib/TraccarAPI";
-import {
-  ADD_GROUP_SUCCESS,
-  ADD_GROUP_FAILED,
-  GET_USER_GROUPS_SUCCESS,
-  GET_USER_GROUPS_FAILED,
-  RESET_USER_GROUPS_LIST
-} from "./actionTypes";
+import { GET_USER_GROUPS_SUCCESS, RESET_USER_GROUPS_LIST } from "./actionTypes";
 
-const addGroupFailed = () => {
-  return {
-    type: ADD_GROUP_FAILED,
-    errorMessage: "There was an error adding the device."
-  };
-};
+import { requestSuccess, requestFailed } from "./genericActions";
 
-const addGroupSuccess = () => {
-  return { type: ADD_GROUP_SUCCESS };
-};
+const cred = state =>
+  Object.assign({}, { email: state.User.email, password: state.User.password });
 
 const getUserGroupsSuccess = payload => {
   return { type: GET_USER_GROUPS_SUCCESS, payload };
 };
 
-const getUserGroupsFailed = () => {
-  return { type: GET_USER_GROUPS_FAILED };
+export const resetUserGroupsList = () => {
+  return { type: RESET_USER_GROUPS_LIST };
 };
 
 export const addGroup = payload => {
   return (dispatch, getState) => {
-    const client = TraccarAPI({
-      email: getState().User.email,
-      password: getState().User.password
-    });
+    const client = TraccarAPI(cred(getState()));
     client
       .post("/groups", payload)
-      .then(res => {
-        dispatch(addGroupSuccess());
-      })
-      .catch(error => {
-        console.error(error);
-        dispatch(addGroupFailed());
-      });
+      .then(() => dispatch(requestSuccess()))
+      .catch(error => requestFailed(error));
   };
 };
 
 export const userGroups = () => {
   return (dispatch, getState) => {
-    const client = TraccarAPI({
-      email: getState().User.email,
-      password: getState().User.password
-    });
+    const client = TraccarAPI(cred(getState()));
     client
       .get(`/groups?userId=${getState().User.id}`)
-      .then(res => {
-        dispatch(getUserGroupsSuccess(res.data));
-      })
-      .catch(error => {
-        console.error(error);
-        dispatch(getUserGroupsFailed());
-      });
+      .then(res => dispatch(getUserGroupsSuccess(res.data)))
+      .catch(error => dispatch(requestFailed(error)));
   };
-};
-
-export const resetUserGroupsList = () => {
-  return { type: RESET_USER_GROUPS_LIST };
 };
