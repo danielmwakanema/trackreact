905b04275962ae81acfd3e86377b5bc5c6cdabf9
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-15 14:02:01 +0200

    refactor(Actions): Use generic request actions

--- src/Redux/actions/driverActions.js
+++ src/Redux/actions/driverActions.js
@@ -1,61 +1,38 @@
 import TraccarAPI from "../../lib/TraccarAPI";
-import { ADD_DRIVER_SUCCESS, ADD_DRIVER_FAILED, GET_USER_DRIVERS_SUCCESS, GET_USER_DRIVERS_FAILED, RESET_USER_DRIVERS_LIST } from "./actionTypes";
+import {
+  GET_USER_DRIVERS_SUCCESS,
+  RESET_USER_DRIVERS_LIST
+} from "./actionTypes";
 
-const addDriverFailed = () => {
-  return {
-    type: ADD_DRIVER_FAILED,
-    errorMessage: "There was an error adding the driver."
-  };
-};
+import { requestSuccess, requestFailed } from "./genericActions";
 
-const addDriverSuccess = () => {
-  return { type: ADD_DRIVER_SUCCESS };
-};
+const cred = state =>
+  Object.assign({}, { email: state.User.email, password: state.User.password });
 
 const getUserDriversSuccess = drivers => {
-  return { type: GET_USER_DRIVERS_SUCCESS, payload: { drivers: drivers } }
-}
+  return { type: GET_USER_DRIVERS_SUCCESS, payload: { drivers: drivers } };
+};
 
-const getUserDriversFailed = () => {
-  return { type: GET_USER_DRIVERS_FAILED }
-}
+export const resetUserDriversList = () => {
+  return { type: RESET_USER_DRIVERS_LIST };
+};
 
 export const addDriver = payload => {
   return (dispatch, getState) => {
-    const client = TraccarAPI({
-      email: getState().User.email,
-      password: getState().User.password
-    });
+    const client = TraccarAPI(cred(getState()));
     client
       .post("/drivers", payload)
-      .then(res => {
-        dispatch(addDriverSuccess());
-      })
-      .catch(error => {
-        console.error(error);
-        dispatch(addDriverFailed());
-      });
+      .then(() => dispatch(requestSuccess()))
+      .catch(error => dispatch(requestFailed(error)));
   };
 };
 
 export const userDrivers = () => {
   return (dispatch, getState) => {
-    const client = TraccarAPI({
-      email: getState().User.email,
-      password: getState().User.password
-    });
+    const client = TraccarAPI(cred(getState()));
     client
       .get(`/drivers?userId=${getState().User.id}`)
-      .then(res => {
-        dispatch(getUserDriversSuccess(res.data));
-      })
-      .catch(error => {
-        console.error(error);
-        dispatch(getUserDriversFailed());
-      });
+      .then(res => dispatch(getUserDriversSuccess(res.data)))
+      .catch(error => dispatch(requestFailed(error)));
   };
-}
-
-export const resetUserDriversList = () => {
-  return { type: RESET_USER_DRIVERS_LIST }
-}
+};
