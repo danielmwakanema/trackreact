52e6b615c56c1ce2be30fa99afa64e70c0fbb558
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-15 14:02:18 +0200

    refactor(Actions): Use generic request actions

--- src/Redux/actions/maintenanceActions.js
+++ src/Redux/actions/maintenanceActions.js
@@ -1,67 +1,41 @@
 import TraccarAPI from "../../lib/TraccarAPI";
 import {
-  ADD_MAINTENANCE_SUCCESS,
-  ADD_MAINTENANCE_FAILED,
   GET_USER_MAINTENANCES_SUCCESS,
-  GET_USER_MAINTENANCES_FAILED,
   RESET_USER_MAINTENANCES_LIST
 } from "../actions/actionTypes";
 
-const addMaintenanceFailed = () => {
-  return {
-    type: ADD_MAINTENANCE_FAILED,
-    errorMessage: "There was an error adding the device."
-  };
-};
+import { requestSuccess, requestFailed } from "./genericActions";
 
-const addMaintenanceSuccess = () => {
-  return { type: ADD_MAINTENANCE_SUCCESS };
-};
+const cred = state =>
+  Object.assign({}, { email: state.User.email, password: state.User.password });
 
 const getUserMaintenancesSuccess = payload => {
-  return { type: GET_USER_MAINTENANCES_SUCCESS, payload: { maintenances: payload } };
-};
-
-const getUserMaintenancesFailed = () => {
-  return { type: GET_USER_MAINTENANCES_FAILED };
-};
-
-export const addMaintenance = payload => {
-  return (dispatch, getState) => {
-    const client = TraccarAPI({
-      email: getState().User.email,
-      password: getState().User.password
-    });
-    client
-      .post("/maintenance", payload)
-      .then(res => {
-        dispatch(addMaintenanceSuccess());
-      })
-      .catch(error => {
-        console.error(error);
-        dispatch(addMaintenanceFailed());
-      });
-  };
-};
-
-export const userMaintenances = () => {
-  return (dispatch, getState) => {
-    const client = TraccarAPI({
-      email: getState().User.email,
-      password: getState().User.password
-    });
-    client
-      .get(`/maintenance?userId=${getState().User.id}`)
-      .then(res => {
-        dispatch(getUserMaintenancesSuccess(res.data));
-      })
-      .catch(error => {
-        console.error(error);
-        dispatch(getUserMaintenancesFailed());
-      });
+  return {
+    type: GET_USER_MAINTENANCES_SUCCESS,
+    payload: { maintenances: payload }
   };
 };
 
 export const resetUserMaintenancesList = () => {
   return { type: RESET_USER_MAINTENANCES_LIST };
+};
+
+export const addMaintenance = payload => {
+  return (dispatch, getState) => {
+    const client = TraccarAPI(cred(getState()));
+    client
+      .post("/maintenance", payload)
+      .then(() => dispatch(requestSuccess()))
+      .catch(error => requestFailed(error));
+  };
+};
+
+export const userMaintenances = () => {
+  return (dispatch, getState) => {
+    const client = TraccarAPI(cred(getState()));
+    client
+      .get(`/maintenance?userId=${getState().User.id}`)
+      .then(res => dispatch(getUserMaintenancesSuccess(res.data)))
+      .catch(error => requestFailed(error));
+  };
 };
