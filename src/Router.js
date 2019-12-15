c066490fc8655cac7adc97a4499827ba710c8ca2
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-15 13:57:34 +0200

    feat(Users): Gatekeep routes

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
-import AdminLayout from './layouts/Admin/Admin';
-import Login from './views/User/Login';
+import AdminLayout from "./layouts/Admin/Admin";
+import Login from "./views/User/Login";
 
 const hist = createBrowserHistory();
+const tokensNotEmpty = tokens =>
+  tokens.reduce((acc, val) => acc && val !== null && val !== "", true);
 
-export default () => {
+const toLogin = () => {
+  return (
+    <Router history={hist}>
+      <Switch>
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Redirect from="*" to="/login" />
+      </Switch>
+    </Router>
+  );
+};
+
+const passage = () => {
   return (
     <Router history={hist}>
       <Switch>
         <Route path="/admin" render={props => <AdminLayout {...props} />} />
-        <Route path="/login" render={props => <Login {...props} /> } />
+        <Route path="/login" render={props => <Login {...props} />} />
         <Redirect from="/" to="/login" />
       </Switch>
     </Router>
-  )
-}
+  );
+};
+
+export default () => {
+  const tokens = useSelector(state =>
+    Object.assign(
+      {},
+      { email: state.User.email, password: state.User.password }
+    )
+  );
+  const valid = tokensNotEmpty(Object.values(tokens));
+
+  if (valid) return passage();
+  return toLogin();
+};
