8a0c5979a55b67a7adc0d20b612af0c5843d50a4
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-15 14:03:37 +0200

    refactor(Reducers): Drop unused paths

--- src/Redux/reducers/geofencesReducer.js
+++ src/Redux/reducers/geofencesReducer.js
@@ -1,8 +1,5 @@
 import {
-  ADD_GEOFENCE_SUCCESS,
-  ADD_GEOFENCE_FAILED,
   GET_USER_GEOFENCES_SUCCESS,
-  GET_USER_GEOFENCES_FAILED,
   RESET_USER_GEOFENCES_LIST
 } from "../actions/actionTypes";
 
@@ -13,14 +10,8 @@ const init = {
 
 export default (state = init, action) => {
   switch (action.type) {
-    case ADD_GEOFENCE_SUCCESS:
-      return Object.assign({}, state);
-    case ADD_GEOFENCE_FAILED:
-      return Object.assign({}, state);
     case GET_USER_GEOFENCES_SUCCESS:
       return Object.assign({}, state, action.payload);
-    case GET_USER_GEOFENCES_FAILED:
-      return Object.assign({}, state);
     case RESET_USER_GEOFENCES_LIST:
       return Object.assign({}, state, { geofences: [] });
     default:
