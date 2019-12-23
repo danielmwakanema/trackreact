70745edca6ee30cd5bbf3cd97cb3feb739782bc5
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-23 15:14:58 +0200

    feat(TripPlot): Add device trips reducer

--- src/Redux/reducers/devicesReducer.js
+++ src/Redux/reducers/devicesReducer.js
@@ -1,10 +1,14 @@
+import uuid from 'uuid/v1';
+
 import {
   GET_USER_DEVICES_SUCCESS,
   RESET_USER_DEVICES_LIST,
+  GET_DEVICE_TRIPS_SUCCESS
 } from "../actions/actionTypes";
 
 const init = {
-  devices: []
+  devices: [],
+  deviceTrips: []
 };
 
 export default (state = init, action) => {
@@ -13,6 +17,8 @@ export default (state = init, action) => {
       return Object.assign({}, state, action.payload);
     case RESET_USER_DEVICES_LIST:
       return Object.assign({}, state, { devices: [] });
+    case GET_DEVICE_TRIPS_SUCCESS:
+      return Object.assign({}, state, { deviceTrips: action.payload.map(trip => Object.assign({}, trip, { id: uuid() })) })
     default:
       return state;
   }
