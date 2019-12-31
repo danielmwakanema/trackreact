e354e1ec69173b2f5d09eb6c07e56b46407ec7b2
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-31 12:00:50 +0200

    feat(UserMgmt): Add form field map util

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,4 +7,17 @@ export default class {
   static formFieldValues (fieldIds) {
     return fieldIds.map(id => document.getElementById(id).value) 
   }
+
+  /**
+   * Function to extract form field values given field ids
+   * @param {Array} fieldIds
+   * @return {Array}
+   */
+  static formFieldMap (fieldIds) {
+    return fieldIds.reduce((acc, val) => {
+      const data = document.getElementById(val).value
+      acc[val] = data
+      return acc
+    }, {}) 
+  }
 }
