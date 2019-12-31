bda52d4d743f575ac84973214bb53885d2defb8e
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-31 12:01:37 +0200

    refactor(DeviceMgmt): Trim stateful code

--- src/views/Devices/Edit.jsx
+++ src/views/Devices/Edit.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 
 // reactstrap components
 import {
@@ -7,7 +8,6 @@ import {
   CardHeader,
   CardBody,
   CardFooter,
-  CardText,
   FormGroup,
   Form,
   Input,
@@ -15,13 +15,39 @@ import {
   Col
 } from "reactstrap";
 
+import { updateDevice } from "../../Redux/actions/deviceActions";
+import Utils from "../../utils";
+
 class Edit extends React.Component {
+  FIELD_IDS = [
+    "uniqueId",
+    "name",
+    "status",
+    "disabled",
+    "model",
+    "category",
+  ];
+
+  handleSubmit = () => {
+    const data = this.params();
+    if (this.paramsAreValid(Object.values(data))) {
+      const id = this.props.device.id;
+      const merge =Object.assign({}, this.props.device, data);
+      this.props.updateDevice(id, merge)
+    } else alert("The supplied information is invalid.");
+  };
+
+  params = () => Utils.formFieldMap(this.FIELD_IDS);
+
+  paramsAreValid = (par = []) =>
+    par.reduce((acc, val) => val !== "" && val !== undefined && acc, true);
+
   render() {
     return (
       <>
         <div className="content">
           <Row>
-            <Col md="12">
+            <Col md="7">
               <Card>
                 <CardHeader>
                   <h5 className="title">Edit Device</h5>
@@ -29,109 +55,87 @@ class Edit extends React.Component {
                 <CardBody>
                   <Form>
                     <Row>
-                      <Col className="pr-md-1" md="5">
+                      <Col className="pr-md-1" md="11">
                         <FormGroup>
-                          <label>Company (disabled)</label>
+                          <label>Unique ID</label>
                           <Input
-                            defaultValue="Creative Code Inc."
-                            disabled
-                            placeholder="Company"
+                            defaultValue={this.props.device.uniqueId}
+                            placeholder="Unique identifier"
                             type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="px-md-1" md="3">
-                        <FormGroup>
-                          <label>Username</label>
-                          <Input
-                            defaultValue="michael23"
-                            placeholder="Username"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="pl-md-1" md="4">
-                        <FormGroup>
-                          <label htmlFor="exampleInputEmail1">
-                            Email address
-                          </label>
-                          <Input placeholder="mike@email.com" type="email" />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col className="pr-md-1" md="6">
-                        <FormGroup>
-                          <label>First Name</label>
-                          <Input
-                            defaultValue="Mike"
-                            placeholder="Company"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="pl-md-1" md="6">
-                        <FormGroup>
-                          <label>Last Name</label>
-                          <Input
-                            defaultValue="Andrew"
-                            placeholder="Last Name"
-                            type="text"
+                            id="uniqueId"
+                            name="uniqueId"
                           />
                         </FormGroup>
                       </Col>
                     </Row>
                     <Row>
-                      <Col md="12">
+                      <Col className="pr-md-1" md="11">
                         <FormGroup>
-                          <label>Address</label>
+                          <label>Name</label>
                           <Input
-                            defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                            placeholder="Home Address"
+                            defaultValue={this.props.device.name}
+                            placeholder="Name"
                             type="text"
+                            id="name"
+                            name="name"
                           />
                         </FormGroup>
                       </Col>
                     </Row>
                     <Row>
-                      <Col className="pr-md-1" md="4">
+                      <Col className="pr-md-1" md="11">
                         <FormGroup>
-                          <label>City</label>
-                          <Input
-                            defaultValue="Mike"
-                            placeholder="City"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="px-md-1" md="4">
-                        <FormGroup>
-                          <label>Country</label>
-                          <Input
-                            defaultValue="Andrew"
-                            placeholder="Country"
-                            type="text"
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col className="pl-md-1" md="4">
-                        <FormGroup>
-                          <label>Postal Code</label>
-                          <Input placeholder="ZIP Code" type="number" />
+                          <label>Status</label>
+                          <select
+                            id="status"
+                            name="status"
+                            className={"form-control"}
+                          >
+                            <option value="Active">Active</option>
+                            <option value="Inactive">Inactive</option>
+                          </select>
                         </FormGroup>
                       </Col>
                     </Row>
                     <Row>
-                      <Col md="8">
+                      <Col className="pr-md-1" md="11">
                         <FormGroup>
-                          <label>About Me</label>
+                          <label>Disabled</label>
+                          <select
+                            id="disabled"
+                            name="disabled"
+                            className={"form-control"}
+                          >
+                            <option value="false">No</option>
+                            <option value="true">Yes</option>
+                          </select>
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-md-1" md="11">
+                        <FormGroup>
+                          <label>Model</label>
                           <Input
-                            cols="80"
-                            defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in
-                            that two seat Lambo."
-                            placeholder="Here can be your description"
-                            rows="4"
-                            type="textarea"
+                            defaultValue={this.props.device.model}
+                            type="text"
+                            placeholder="Model"
+                            id="model"
+                            name="model"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="pr-md-1" md="11">
+                        <FormGroup>
+                          <label>Category</label>
+                          <Input
+                            defaultValue={this.props.device.category}
+                            type="text"
+                            placeholder="Category"
+                            id="category"
+                            name="category"
                           />
                         </FormGroup>
                       </Col>
@@ -139,7 +143,12 @@ class Edit extends React.Component {
                   </Form>
                 </CardBody>
                 <CardFooter>
-                  <Button className="btn-fill" color="primary" type="submit">
+                  <Button
+                    className="btn-fill"
+                    color="primary"
+                    type="submit"
+                    onClick={this.handleSubmit}
+                  >
                     Save
                   </Button>
                 </CardFooter>
@@ -152,4 +161,17 @@ class Edit extends React.Component {
   }
 }
 
-export default Edit;
+const mapStateToProps = state => ({ device: state.Device.device });
+
+const mapDispatchToProps = dispatch => {
+  return {
+    updateDevice: (id, payload) => {
+      dispatch(updateDevice(id, payload));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Edit);
