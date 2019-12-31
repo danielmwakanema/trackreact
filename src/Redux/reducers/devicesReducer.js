6a90a7f9a34b02fe81d9d8f73f84472bc5a72741
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-31 11:59:04 +0200

    refactor(DeviceMgmt): Add set device reducer

--- src/Redux/reducers/devicesReducer.js
+++ src/Redux/reducers/devicesReducer.js
@@ -3,12 +3,14 @@ import uuid from 'uuid/v1';
 import {
   GET_USER_DEVICES_SUCCESS,
   RESET_USER_DEVICES_LIST,
-  GET_DEVICE_TRIPS_SUCCESS
+  GET_DEVICE_TRIPS_SUCCESS,
+  SET_DEVICE
 } from "../actions/actionTypes";
 
 const init = {
   devices: [],
-  deviceTrips: []
+  deviceTrips: [],
+  device: null
 };
 
 export default (state = init, action) => {
@@ -18,7 +20,9 @@ export default (state = init, action) => {
     case RESET_USER_DEVICES_LIST:
       return Object.assign({}, state, { devices: [] });
     case GET_DEVICE_TRIPS_SUCCESS:
-      return Object.assign({}, state, { deviceTrips: action.payload.map(trip => Object.assign({}, trip, { id: uuid() })) })
+      return Object.assign({}, state, { deviceTrips: action.payload.map(trip => Object.assign({}, trip, { id: uuid() })) });
+    case SET_DEVICE:
+      return Object.assign({}, state, { device: action.payload });
     default:
       return state;
   }
