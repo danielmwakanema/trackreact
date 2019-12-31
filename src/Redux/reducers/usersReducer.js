805926c615fe307c490e164c78e35dbb60dcd20e
Daniel Mwakanema <danielmwakanema95@gmail.com>
2019-12-31 11:59:46 +0200

    feat(UserMgmt): Improve reducer

--- src/Redux/reducers/usersReducer.js
+++ src/Redux/reducers/usersReducer.js
@@ -1,17 +1,23 @@
-import { LOGIN_SUCCESS, RESET_CREDENTIALS } from "../actions/actionTypes";
+import { LOGIN_SUCCESS, RESET_CREDENTIALS, GET_USERS_SUCCESS, SET_USER } from "../actions/actionTypes";
 
 const init = {
   id: null,
   email: "",
-  password: ""
+  password: "",
+  users: [],
+  user: null
 };
 
 export default (state = init, action) => {
   switch (action.type) {
     case LOGIN_SUCCESS:
       return Object.assign({}, state, action.payload);
+    case GET_USERS_SUCCESS:
+      return Object.assign({}, state, { users: action.payload });
+    case SET_USER:
+      return Object.assign({}, state, { user: action.payload });
     case RESET_CREDENTIALS:
-      return Object.assign({}, state, { id: null, email: "", password: "" })
+      return Object.assign({}, state, { id: null, email: "", password: "" });
     default:
       return state;
   }
