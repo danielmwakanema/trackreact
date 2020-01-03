465d16ff43f5ace3af0620bf481ec7d7a32d5062
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-01-03 09:41:52 +0200

    feat(DriverMgmt): Add view route

--- src/Routes/Drivers.js
+++ src/Routes/Drivers.js
@@ -1,5 +1,6 @@
 import Add from '../views/Drivers/Add';
 import Edit from '../views/Drivers/Edit';
+import View from '../views/Drivers/View';
 
 export default [
   {
@@ -14,6 +15,13 @@ export default [
     name: "Edit Driver",
     icon: "tim-icons icon-pencil",
     component: Edit,
+    layout: "/admin"
+  },
+  {
+    path: "/driver/view/:id",
+    name: "View Driver",
+    icon: "tim-icons icon-pencil",
+    component: View,
     layout: "/admin"
   }
 ]
