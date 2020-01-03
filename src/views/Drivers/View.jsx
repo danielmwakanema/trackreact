7177632da706984d7633bb0e4ff6b0b17c219e2a
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-01-03 09:43:52 +0200

    feat(DriverMgmt): Add driver view page

--- src/views/Drivers/View.jsx
+++ src/views/Drivers/View.jsx
@@ -1,201 +1,147 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-
-// reactstrap components
 import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  FormGroup,
-  Form,
-  Input,
   Row,
-  Col
+  Col,
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  UncontrolledDropdown,
+  DropdownMenu,
+  DropdownItem,
+  Button
 } from "reactstrap";
 
-import { viewDevice } from "../../Redux/actions/deviceActions";
+import TraccarAPI from "../../lib/TraccarAPI";
+import { showNotification } from "../../Redux/actions/notificationActions";
+import { setDriver } from "../../Redux/actions/driverActions";
 
-class View extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.inputIsValid = this.inputIsValid.bind(this);
-    this.prepPayload = this.prepPayload.bind(this);
-    this.createDevice = this.createDevice.bind(this);
-  }
-
-  componentDidMount() {
-    this.props.viewDevice(this.props.match.params.id);
-  }
-
-  handleInputChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  async createDevice() {
-  }
-
-  async handleSubmit(event) {
-    event.preventDefault();
-    if (this.inputIsValid()) {
-      await this.createDevice();
-    } else {
-      alert("The specified input is not valid.");
-    }
-  }
-
-  inputIsValid() {
-    for (const [, val] of Object.entries(this.state)) {
-      if (val === "" || val === undefined) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  prepPayload = () => {
-    return {
-      name: this.state.name,
-      uniqueId: this.state.uniqueId,
-      status: document.getElementById("status").value,
-      disabled: Boolean(document.getElementById("disabled").value),
-      category: this.state.category,
-      model: this.state.model
-    };
-  };
-
-  render() {
-    return (
-      <>
-        <div className="content">
-          <Row>
-            <Col md="7">
-              <Card>
-                <CardHeader>
-                  <h5 className="title">View Device</h5>
-                </CardHeader>
-                <CardBody>
-                  <Form>
-                    <Row>
-                      <Col className="pr-md-1" md="11">
-                        <FormGroup>
-                          <label>Unique ID</label>
-                          <Input
-                            defaultValue=""
-                            placeholder="Unique identifier"
-                            type="text"
-                            name="uniqueId"
-                            onChange={this.handleInputChange}
-                          />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col className="pr-md-1" md="11">
-                        <FormGroup>
-                          <label>Name</label>
-                          <Input
-                            defaultValue=""
-                            placeholder="Name"
-                            type="text"
-                            name="name"
-                            onChange={this.handleInputChange}
-                          />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col className="pr-md-1" md="11">
-                        <FormGroup>
-                          <label>Status</label>
-                          <select
-                            id="status"
-                            name="status"
-                            onChange={this.handleInputChange}
-                            className={"form-control"}
-                          >
-                            <option value="Active">Active</option>
-                            <option value="Inactive">Inactive</option>
-                          </select>
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col className="pr-md-1" md="11">
-                        <FormGroup>
-                          <label>Disabled</label>
-                          <select
-                            id="disabled"
-                            name="disabled"
-                            onChange={this.handleInputChange}
-                            className={"form-control"}
-                          >
-                            <option value="true">Yes</option>
-                            <option value="false">No</option>
-                          </select>
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col className="pr-md-1" md="11">
-                        <FormGroup>
-                          <label>Model</label>
-                          <Input
-                            defaultValue=""
-                            type="text"
-                            placeholder="XAB112"
-                            name="model"
-                            onChange={this.handleInputChange}
-                          />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col className="pr-md-1" md="11">
-                        <FormGroup>
-                          <label>Category</label>
-                          <Input
-                            defaultValue=""
-                            type="text"
-                            placeholder="Cab"
-                            name="category"
-                            onChange={this.handleInputChange}
-                          />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                  </Form>
-                </CardBody>
-                <CardFooter>
-                  <Button
-                    className="btn-fill"
-                    color="primary"
-                    type="submit"
-                    onClick={this.handleSubmit}
-                  >
-                    Save
-                  </Button>
-                </CardFooter>
-              </Card>
-            </Col>
-          </Row>
-        </div>
-      </>
-    );
-  }
-}
+import "./View.css";
 
 const mapDispatchToProps = dispatch => {
   return {
-    viewDevice: deviceId => dispatch(viewDevice(deviceId))
+    showNotification: payload => dispatch(showNotification(payload)),
+    setDriver: driver => dispatch(setDriver(driver))
   };
 };
 
+const mapStateToProps = state => ({
+  auth: { email: state.User.email, password: state.User.password },
+  drivers: state.Driver.drivers
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
-)(View);
+)(
+  class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        id: Number(this.props.match.params.id),
+        driver: null,
+        client: TraccarAPI(this.props.auth)
+      };
+    }
+
+    componentDidMount() {
+      try {
+        const driver = this.get(this.state.id);
+        this.setState(Object.assign({}, this.state, { driver }));
+      } catch (e) {
+        this.error(e);
+      }
+    }
+
+    get = id => this.props.drivers.filter(driver => driver.id === id)[0];
+
+    edit = () => {
+      const driver = this.state.driver;
+      this.props.setDriver(driver);
+      this.props.history.push(`/admin/driver/edit/${driver.id}`);
+    };
+
+    delete = () => {
+      this.state.client.delete(`/drivers/${this.state.driver.id}`)
+        .then(() => this.props.history.push("/admin/drivers"))
+        .catch(this.error);
+    };
+
+    error = error => this.props.showNotification({ title: 'Error', message: error.message});
+
+    render() {
+      return (
+        <>
+          <div className="content">
+            {this.state.driver !== null && (
+              <Row>
+                <Col md="12">
+                  <Card>
+                    <CardBody>
+                      <CardTitle>
+                        <h4 className>
+                          {this.state.driver.name} (ID: {this.state.driver.uniqueId})
+                        </h4>
+                        <UncontrolledDropdown>
+                          <Button
+                            className="btn-round btn-icon"
+                            color="info"
+                            caret
+                            data-toggle="dropdown"
+                          >
+                            <i className="tim-icons icon-settings" />
+                          </Button>
+                          <DropdownMenu>
+                            <DropdownItem onClick={this.edit}>
+                              Edit
+                            </DropdownItem>
+                            <DropdownItem onClick={this.delete}>
+                              Delete
+                            </DropdownItem>
+                          </DropdownMenu>
+                        </UncontrolledDropdown>
+                      </CardTitle>
+                      <CardText>
+                        <hr />
+                        <table>
+                          <tbody>
+                            <tr>
+                              <td>License Number:</td>
+                              <td>
+                                {this.state.driver.attributes.licenseNumber}
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>License Expiry Date:</td>
+                              <td>
+                                {this.state.driver.attributes.licenseExpiryDate}
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Home Address:</td>
+                              <td>
+                                {this.state.driver.attributes.homeAddress}
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </CardText>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            )}
+            {this.state.driver === null && (
+              <Row>
+                <Col md="8">
+                  <h4>No driver data at the moment.</h4>
+                </Col>
+              </Row>
+            )}
+          </div>
+        </>
+      );
+    }
+  }
+);
