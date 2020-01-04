e93f661db0fa829ed357a95abf964e23e755a6a2
Daniel Mwakanema <danielmwakanema95@gmail.com>
2020-01-04 08:23:31 +0200

    chore(Visualization): Add default car icon

--- src/components/Maps/NormalMap.jsx
+++ src/components/Maps/NormalMap.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import { withScriptjs } from 'react-google-maps/lib';
 
+import asset from "assets/img/icons8-car-100.png";
+
 const ZOOM = 17
 
 const validCoordinates = (lat, lng) => lat && lng
@@ -11,7 +13,7 @@ export default withScriptjs(
   withGoogleMap(props => (
     <GoogleMap defaultZoom={ZOOM} center={props.center}>
       { props.devices.length >= 0 && (props.devices.map((device, index) =>
-        validCoordinates(device.lat, device.lng && <Marker key={index} position={{ lat: device.lat, lng: device.lng }} title={`${device.name}`} />)))
+        validCoordinates(device.lat, device.lng && <Marker icon={asset} key={index} position={{ lat: device.lat, lng: device.lng }} title={`${device.name}`} />)))
       }
     </GoogleMap>
   ))
